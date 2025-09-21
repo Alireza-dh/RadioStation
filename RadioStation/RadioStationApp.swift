25f87d3312bca3b249e0490a65f333963cb38307
Alireza <ar.dosthoseini@gmail.com>
2025-09-21 10:34:29 +0200

    Initial Commit

---
+++ RadioStation/RadioStationApp.swift
@@ -0,0 +1,17 @@
+//
+//  RadioStationApp.swift
+//  RadioStation
+//
+//  Created by Alireza on 21/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct RadioStationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
